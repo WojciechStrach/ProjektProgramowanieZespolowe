ce3b7ee2bb52cf7969be116def14b0367c94f8dd
WojciechStrach <wojciech.strach1@gmail.com>
2017-04-17 22:33:53 +0200

    DAO do reszty modeli

--- kod/src/main/java/zespolowe/model/TasksDAO.java
+++ kod/src/main/java/zespolowe/model/TasksDAO.java
@@ -1,4 +1,147 @@
 package zespolowe.model;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import zespolowe.Utilize.DatabaseHandler;
+
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class TasksDAO {
+
+    private static ObservableList<Tasks> getTasksList(ResultSet rs) throws SQLException, ClassNotFoundException {
+
+        ObservableList<Tasks> tasksList = FXCollections.observableArrayList();
+
+        while (rs.next()) {
+            Tasks task = new Tasks();
+
+            task.setTaskId(rs.getInt("task_id"));
+            task.setProjectId(rs.getInt("project_id"));
+            task.setUserId(rs.getInt("user_id"));
+            task.setDescripition(rs.getString("description"));
+            task.setDateAndTime(rs.getDate("dateAndTime"));
+            task.setState(rs.getInt("state"));
+
+            tasksList.add(task);
+        }
+
+        return tasksList;
+    }
+
+    public static ObservableList<Tasks> getAllTasks () throws SQLException, ClassNotFoundException {
+
+        String selectStmt = "SELECT * FROM tasks";
+
+        try {
+
+            ResultSet rsTasks = DatabaseHandler.databaseExecuteQuery(selectStmt);
+
+            ObservableList<Tasks> tasksList = getTasksList(rsTasks);
+
+            return tasksList;
+
+        } catch (Exception e) {
+            System.out.println("Nie udało się pobrać zadań: " + e);
+            e.printStackTrace();
+
+            return null;
+        }
+    }
+
+    public static ObservableList<Tasks> searchTasks (int data) throws SQLException, ClassNotFoundException {
+
+        String selectStmt = "SELECT * " +
+                "FROM tasks " +
+                "WHERE task_id =" + data +
+                "OR project_id =" + data +
+                "OR user_id =" + data +
+                "OR state =" + data;
+
+        try {
+            ResultSet rsTasks = DatabaseHandler.databaseExecuteQuery(selectStmt);
+
+            ObservableList<Tasks> tasksList = getTasksList(rsTasks);
+
+            return tasksList;
+
+        } catch (Exception e) {
+            System.out.println("Wystąpił błąd podczas wyszukiwania zadań " + e);
+            e.printStackTrace();
+
+            return null;
+        }
+    }
+
+    public static ObservableList<Tasks> searchTasks (String data) throws SQLException, ClassNotFoundException {
+
+        String selectStmt = "SELECT * " +
+                "FROM tasks " +
+                "WHERE description =" + data;
+
+        try {
+            ResultSet rsTasks = DatabaseHandler.databaseExecuteQuery(selectStmt);
+
+            ObservableList<Tasks> tasksList = getTasksList(rsTasks);
+
+            return tasksList;
+
+        } catch (Exception e) {
+            System.out.println("Wystąpił błąd podczas wyszukiwania zadań " + e);
+            e.printStackTrace();
+
+            return null;
+        }
+    }
+
+    public static ObservableList<Tasks> searchTasks (Date data) throws SQLException, ClassNotFoundException {
+
+        String selectStmt = "SELECT * " +
+                "FROM tasks " +
+                "WHERE dateAndTime =" + data;
+
+        try {
+            ResultSet rsTasks = DatabaseHandler.databaseExecuteQuery(selectStmt);
+
+            ObservableList<Tasks> tasksList = getTasksList(rsTasks);
+
+            return tasksList;
+
+        } catch (Exception e) {
+            System.out.println("Wystąpił błąd podczas wyszukiwania zadań " + e);
+            e.printStackTrace();
+
+            return null;
+        }
+    }
+
+    public static void insertTask (int projectId, int userId, String description, Date dateAndTime, int state) throws SQLException, ClassNotFoundException {
+
+        String updateStmt =
+                "INSERT INTO tasks" +
+                        "(project_id, user_id, description, dateAndTime, state)" +
+                        "VALUES" +
+                        "( " +projectId+ "," +userId+ ",'" +description+ "'," +dateAndTime+ "," +state+ ")";
+
+        try {
+            DatabaseHandler.databaseExecuteUpdate(updateStmt);
+        } catch (Exception e) {
+            System.out.print("Nie udało się dodać zadania, nie patrz na stack trace bo dostaniesz raka " + e);
+            e.printStackTrace();
+        }
+    }
+
+    public static void deleteTask (int id) throws SQLException, ClassNotFoundException {
+
+        String updateStmt =
+                "DELETE FROM tasks" +
+                        "WHERE task_id =" + id;
+        try {
+            DatabaseHandler.databaseExecuteUpdate(updateStmt);
+        } catch (SQLException e) {
+            System.out.print("Nie udało się usunąć zadania: " + e);
+            e.printStackTrace();
+        }
+    }
 }
--- kod/src/main/java/zespolowe/model/UsersDAO.java
+++ kod/src/main/java/zespolowe/model/UsersDAO.java
@@ -1,4 +1,140 @@
 package zespolowe.model;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import zespolowe.Utilize.DatabaseHandler;
+
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class UsersDAO {
+
+    private static ObservableList<Users> getUsersList(ResultSet rs) throws SQLException, ClassNotFoundException {
+
+        ObservableList<Users> usersList = FXCollections.observableArrayList();
+
+        while (rs.next()) {
+            Users user = new Users();
+
+            user.setUserId(rs.getInt("user_id"));
+            user.setEmail(rs.getString("email"));
+            user.setPassword(rs.getString("password"));
+            user.setDisplayName(rs.getString("description"));
+
+            usersList.add(user);
+        }
+
+        return usersList;
+    }
+
+    private static Users getUserFromResultSet(ResultSet rs) throws SQLException {
+
+        Users user = null;
+
+        if (rs.next()) {
+
+            user = new Users();
+
+            user.setUserId(rs.getInt("user_id"));
+            user.setEmail(rs.getString("email"));
+            user.setPassword(rs.getString("password"));
+            user.setDisplayName(rs.getString("description"));
+
+        }
+
+        return user;
+    }
+
+    public static ObservableList<Users> getAllUsers () throws SQLException, ClassNotFoundException {
+
+        String selectStmt = "SELECT * FROM users";
+
+        try {
+
+            ResultSet rsTasks = DatabaseHandler.databaseExecuteQuery(selectStmt);
+
+            ObservableList<Users> usersList = getUsersList(rsTasks);
+
+            return usersList;
+
+        } catch (Exception e) {
+            System.out.println("Nie udało się pobrać użytkowników: " + e);
+            e.printStackTrace();
+
+            return null;
+        }
+    }
+
+    public static Users searchUsers (int data) throws SQLException, ClassNotFoundException {
+
+        String selectStmt = "SELECT * " +
+                "FROM users " +
+                "WHERE user_id =" + data;
+
+        try {
+            ResultSet rsUsers = DatabaseHandler.databaseExecuteQuery(selectStmt);
+
+            Users user = getUserFromResultSet(rsUsers);
+
+            return user;
+
+        } catch (Exception e) {
+            System.out.println("Wystąpił błąd podczas wyszukiwania użytkowników " + e);
+            e.printStackTrace();
+
+            return null;
+        }
+    }
+
+    public static ObservableList<Users> searchUsers (String data) throws SQLException, ClassNotFoundException {
+
+        String selectStmt = "SELECT * " +
+                "FROM users " +
+                "WHERE email =" + data +
+                "OR display_name =" + data;
+
+        try {
+            ResultSet rsUsers = DatabaseHandler.databaseExecuteQuery(selectStmt);
+
+            ObservableList<Users> usersList = getUsersList(rsUsers);
+
+            return usersList;
+
+        } catch (Exception e) {
+            System.out.println("Wystąpił błąd podczas wyszukiwania użytkowników " + e);
+            e.printStackTrace();
+
+            return null;
+        }
+    }
+
+    public static void insertUser (String email, String password, String displayName) throws SQLException, ClassNotFoundException {
+
+        String updateStmt =
+                "INSERT INTO users" +
+                        "(email, password, display_name)" +
+                        "VALUES" +
+                        "( '" +email+ "','" +password+ "','" +displayName+ "')";
+
+        try {
+            DatabaseHandler.databaseExecuteUpdate(updateStmt);
+        } catch (Exception e) {
+            System.out.print("Nie udało się dodać użytkownika, nie patrz na stack trace bo dostaniesz raka " + e);
+            e.printStackTrace();
+        }
+    }
+
+    public static void deleteUser (int id) throws SQLException, ClassNotFoundException {
+
+        String updateStmt =
+                "DELETE FROM users" +
+                        "WHERE user_id =" + id;
+        try {
+            DatabaseHandler.databaseExecuteUpdate(updateStmt);
+        } catch (SQLException e) {
+            System.out.print("Nie udało się usunąć zadania: " + e);
+            e.printStackTrace();
+        }
+    }
 }
