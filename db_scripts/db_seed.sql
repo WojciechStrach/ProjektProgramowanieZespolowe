fb997684f1e5acca287b41c2dda90a4e676424f7
mzalucka <mzalucka92@gmail.com>
2017-04-17 23:00:52 +0200

    Update db_seed.sql
    Updated database

--- db_scripts/db_seed.sql
+++ db_scripts/db_seed.sql
@@ -7,13 +7,19 @@ INSERT INTO Users (email, password, display_name) VALUES ('marianowakowska@examp
 INSERT INTO Projects (title) VALUES ('Pierwszy projekt');
 INSERT INTO Projects (title) VALUES ('Drugi projekt');
 
-INSERT INTO Tasks (description, dateAndTime, state, user_id, project_id) VALUES ('Utworzenie GUI', '14.02.2012', '1', '1', '1');
-INSERT INTO Tasks (description, dateAndTime, state, user_id, project_id) VALUES ('Utworzenie GUI', '14.02.2012', '1', '2', '1');
-INSERT INTO Tasks (description, dateAndTime, state, user_id, project_id) VALUES ('Dodanie funkcjonalnosci', '14.02.2012', '2', '1', '1');
-INSERT INTO Tasks (description, dateAndTime, state, user_id, project_id) VALUES ('Dodanie funkcjonalnosci', '14.02.2012', '2', '2', '1');
-INSERT INTO Tasks (description, dateAndTime, state, user_id, project_id) VALUES ('Dodanie funkcjonalnocci', '14.02.2012', '1', '3', '1');
-INSERT INTO Tasks (description, dateAndTime, state, user_id, project_id) VALUES ('Utworzenie bazy danych', '14.02.2012', '3', '1', '2');
-INSERT INTO Tasks (description, dateAndTime, state, user_id, project_id) VALUES ('Utworzenie bazy danych', '14.02.2012', '3', '2', '2');
-INSERT INTO Tasks (description, dateAndTime, state, user_id, project_id) VALUES ('Utworzenie bazy danych', '14.02.2012', '2', '3', '2');
-INSERT INTO Tasks (description, dateAndTime, state, user_id, project_id) VALUES ('Modyfikacja bazy', '14.02.2012', '4', '1', '2');
-INSERT INTO Tasks (description, dateAndTime, state, user_id, project_id) VALUES ('Modyfikacja bazy', '14.02.2012', '4', '2', '2');
+INSERT INTO projectsmembers(project_id, user_id, admin) VALUES ('1','1','TRUE');
+INSERT INTO projectsmembers(project_id, user_id, admin) VALUES ('1','2','FALSE');
+INSERT INTO projectsmembers(project_id, user_id, admin) VALUES ('1','3','FALSE');
+INSERT INTO projectsmembers(project_id, user_id, admin) VALUES ('2','4','TRUE');
+INSERT INTO projectsmembers(project_id, user_id, admin) VALUES ('2','5','FALSE');
+
+INSERT INTO Tasks (project_id, user_id, description, dateandtime, state) VALUES ('1', '1', 'Utworzenie GUI', '2017-04-16 19:22:34', '1');
+INSERT INTO Tasks (project_id, user_id, description, dateandtime, state) VALUES ('1', '2', 'Utworzenie GUI', '2017-04-16 20:12:45', '2');
+INSERT INTO Tasks (project_id, user_id, description, dateandtime, state) VALUES ('1', '2', 'Dodanie funkcjonalnosci', '2017-04-16 20:14:27', '1');
+INSERT INTO Tasks (project_id, user_id, description, dateandtime, state) VALUES ('1', '1', 'Dodanie funkcjonalnosci', '2017-04-16 20:38:20', '2');
+INSERT INTO Tasks (project_id, user_id, description, dateandtime, state) VALUES ('1', '1', 'Dodanie funkcjonalnosci', '2017-04-16 22:10:45', '3');
+INSERT INTO Tasks (project_id, user_id, description, dateandtime, state) VALUES ('2', '3', 'Utworzenie bazy danych', '2017-04-17 16:12:45', '1');
+INSERT INTO Tasks (project_id, user_id, description, dateandtime, state) VALUES ('2', '4', 'Utworzenie bazy danych', '2017-04-17 16:42:29', '2');
+INSERT INTO Tasks (project_id, user_id, description, dateandtime, state) VALUES ('2', '5', 'Utworzenie bazy danych', '2017-04-17 16:59:11', '3');
+INSERT INTO Tasks (project_id, user_id, description, dateandtime, state) VALUES ('2', '3', 'Modyfikacja bazy', '2017-04-17 17:18:20', '1');
+INSERT INTO Tasks (project_id, user_id, description, dateandtime, state) VALUES ('2', '4', 'Modyfikacja bazy', '2017-04-17 17:49:45', '2');
